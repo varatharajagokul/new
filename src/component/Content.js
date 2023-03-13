d5820eee0d5cbfdfc5802f7146af5c3be2f804fe
varatharajagokul <varatharajagokul@gmail.com>
2023-03-13 10:25:35 +0530

    changes

---
+++ src/component/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./content.css"
+
+function Content() {
+  return (
+    <div className='style'>Content</div>
+  )
+}
+
+export default Content
---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Content from './Content'
+import Dashboard from './Dashboard'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <Navbar/>
+    <Dashboard/>
+    <Content/>
+    </>
+    
+  )
+}
+
+export default Home
